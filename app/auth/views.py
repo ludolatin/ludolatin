e1b96f9df630fdb67c0b9d18cb477277ab85f486
merelinguist <merelinguist@users.noreply.github.com>
2017-02-19 23:33:54 +0000

    Add answer validation page, add icons, add sounds

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,11 +21,11 @@ def login():
         if user_by_email is not None and \
                 user_by_email.verify_password(form.password.data):
             login_user(user_by_email.seen())
-            return redirect(request.args.get('next') or url_for('quiz.quiz', id=1))
+            return redirect(request.args.get('next') or url_for('quiz.ask', id=1))
         if user_by_name is not None and \
                 user_by_name.verify_password(form.password.data):
             login_user(user_by_name.seen())
-            return redirect(request.args.get('next') or url_for('quiz.quiz', id=1))
+            return redirect(request.args.get('next') or url_for('quiz.ask', id=1))
     return render_template('login.html', form=form)
 
 
--- app/quiz/views.py
+++ app/quiz/views.py
@@ -15,10 +15,10 @@ def _get_user():
 
 @quiz.route('/quiz/<int:id>/', methods=['GET', 'POST'])
 @login_required
-def quiz(id):
+def ask(id):
     # answerlist = AnswerList.query.filter_by(id=id).first_or_404()
-
     form = QuizForm()
+
     # POST request:
     if form.validate_on_submit():
 
@@ -30,14 +30,19 @@ def quiz(id):
         question = EnglishPhrase.query.filter_by(id=question_id).first_or_404()
 
         # Retrieve the answer from the POST request data
-        answer=form.answer.data
+        answer = form.answer.data
 
         # Save to the db via Answer model
         # Answer(answer, answerlist.id, question, _get_user()).save()
-        Answer(answer, question, _get_user()).save()
+        answer = Answer(answer, question, _get_user()).save()
+        print answer
 
         # Reload the page with a GET request
-        return redirect(url_for('quiz.quiz', id=id))
+        response = make_response(
+            redirect(url_for('quiz.validate', id=id))
+        )
+        response.set_cookie('answer_id', str(answer.id))
+        return response
 
     # If it wasn't a POST request, must be a GET, so we arrive here
 
@@ -45,7 +50,7 @@ def quiz(id):
     question = EnglishPhrase.query.order_by(func.random()).first()
 
     # Collection of correct answers previously given, returning just the `text` column
-    correct = Answer.query.with_entities(Answer.text).filter_by(is_correct=True).all()
+    correct = Answer.query.with_entities(Answer.text).filter_by(is_correct=True, creator=_get_user()).all()
     # correct = Answer.query.with_entities(Answer.text).filter_by(answerlist_id=id, is_correct=True).all()
 
     # Convert it to a list, and the list to a set
@@ -85,3 +90,54 @@ def quiz(id):
     # )
     response.set_cookie('question_id', str(question.id))
     return response
+
+
+@quiz.route('/quiz/<int:id>/validate', methods=['GET'])
+@login_required
+def validate(id):
+    form = QuizForm()
+
+    # Retrieve the question_id from the cookie
+    question_id = request.cookies.get('question_id')
+
+    # Retrieve the answer from the cookie
+    answer_id = request.cookies.get('answer_id')
+
+    # Retrieve the question from the database
+    # (passed to the model where used to determine correctness)
+    question = EnglishPhrase.query.filter_by(id=question_id).first_or_404()
+
+    # # Retrieve a random English phrase
+    # question = EnglishPhrase.query.order_by(func.random()).first()
+
+    # Retrieve the question from the database
+    # (passed to the model where used to determine correctness)
+    answer = Answer.query.filter_by(id=answer_id).first()
+
+    # Collection of correct answers previously given, returning just the `text` column
+    correct = Answer.query.with_entities(Answer.text).filter_by(is_correct=True, creator=_get_user()).all()
+    # correct = Answer.query.with_entities(Answer.text).filter_by(answerlist_id=id, is_correct=True).all()
+
+    # Convert it to a list, and the list to a set
+    correct = set([r for r, in correct])
+
+    # The list of latin translations for the current english phrase (normally only one, but can be many)
+    translations = []
+    for translation in question.translations:
+        translations.append(translation.text)
+
+    # True if the set (list of unique) latin translations is not in the set of correct answers
+    unknown = set(translations).isdisjoint(correct)
+
+    # The percentage of questions that have been answered correctly
+    progress = float(len(correct)) / EnglishPhrase.query.count() * 100
+
+    # Rather than returning `render_template`, build a response so that we can attach a cookie to it
+    return render_template(
+        'quiz_validate.html',
+        answer=answer,
+        question=question,
+        unknown=unknown,
+        form=form,
+        progress=progress
+    )
