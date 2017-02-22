b857ac17447343c0aef2a2da17f3fd27f3c7bb58
merelinguist <merelinguist@users.noreply.github.com>
2017-02-22 22:58:19 +0000

    Update manage script

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_script import Manager, Shell, prompt_bool
 from ingenuity import app
 from app import db
 from app import models
-from app.models import User, EnglishPhrase, LatinPhrase, english_latin
+from app.models import User, Sentence, Language
 
 from yaml import load, dump
 try:
@@ -26,13 +26,13 @@ manager.add_command("shell", Shell(make_context=_make_context))
 
 @manager.command
 def users():
-    "List all users"
+    """List all users"""
     print User.query.all()
 
 
 @manager.command
 def add_admin():
-    "Add an admin user"
+    """Add an admin user"""
     user = User(
         email="admin@example.com",
         username="admin",
@@ -46,55 +46,79 @@ def add_admin():
     print "Visit http://localhost/admin, login as admin, "
     print  "promote an existing user to admin, then delete this temporary admin user.\n"
 
+@manager.command
+def initialise_languages():
+    """ Initialise languages"""
+
+    languages = ["English", "Latin"]
+
+    for language in languages:
+        l = (Language.query.filter_by(name=language).first() or Language(
+                name=language,
+            )
+        )
+
+        db.session.add(l)
+        db.session.commit()
+
 
 @manager.command
-def load_data():
-    "Load the content of data.yml into the English / Latin tables"
-    file = open('data.yml')
-    data = load(file, Loader=Loader)
+def load_sentences():
+    """Load the content of data.yml into the English / Latin tables"""
+    filename = open('data.yml')
+    data = load(filename, Loader=Loader)
     print data
     words = data['english-latin']
 
     for english, translations in words.items():
-        e = (EnglishPhrase.query.filter_by(text=english).first() or
-            EnglishPhrase(
-                text=english
+        e = (Sentence.query.filter_by(text=english).first() or Sentence(
+                text=english,
+                language=Language.query.filter_by(name="English").first()
             )
         )
 
         for latin in translations:
-            l = (LatinPhrase.query.filter_by(text=latin).first() or
-                LatinPhrase(
-                    text=latin
+            l = (Sentence.query.filter_by(text=latin).first() or Sentence(
+                    text=latin,
+                    language=Language.query.filter_by(name="Latin").first()
                 )
             )
             e.translations.append(l)
+
         db.session.add(e)
         db.session.commit()
+
+
+@manager.command
+def load_data():
+    """ Initialise languages, Load sentence data"""
+    initialise_languages()
+    load_sentences()
     dump_data()
+
 
 @manager.command
 def dump_data():
-    "Output the content of the English / Latin tables as YAML"
-    english_phrases = EnglishPhrase.query.all()
+    """Output the content of the English / Latin tables as YAML"""
+    english_sentences = Sentence.query.join(Sentence.language).filter(Language.name == "English").all()
     print "english-latin:"
-    for phrase in english_phrases:
-        print "  " + phrase.text + ":"
-        for latin in phrase.translations:
+    for sentence in english_sentences:
+        print "  " + sentence.text + ":"
+        for latin in sentence.translations:
             print "    - " + latin.text
 
 
 @manager.command
 def delete_data():
-    "Delete the content of the English / Latin tables"
+    """Delete the content of the English / Latin tables"""
     if prompt_bool("Are you sure you want to delete all English and Latin phrases?"):
-        EnglishPhrase.query.delete()
-        LatinPhrase.query.delete()
+        Sentence.query.delete()
 
-        # Delete contents of the (Model-less) english_latin association table
-       # db.session.query(english_latin).delete()
+        # Delete contents of the (Model-less) association table
+       # db.session.query(sentence_to_sentence).delete()
        # db.session.commit()
     dump_data()
+
 
 @manager.command
 def db_meta():
