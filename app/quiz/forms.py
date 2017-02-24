573b1cc09f68c56ead5634d8495a3e2ebf3d53e2
merelinguist <merelinguist@users.noreply.github.com>
2017-02-24 00:09:06 +0000

    Replace jumbotron with card

--- app/quiz/forms.py
+++ app/quiz/forms.py
@@ -7,4 +7,4 @@ from wtforms.validators import DataRequired, Length
 
 class QuizForm(FlaskForm):
     answer = StringField('Enter your translation', validators=[DataRequired(), Length(1, 128)])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Check')
