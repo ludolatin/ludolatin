e49f9dc35e4055870c7eabbb60cc09023e13ab6a
merelinguist <merelinguist@users.noreply.github.com>
2017-02-25 02:24:00 +0000

    Add more flexible form error messsaging

--- app/quiz/forms.py
+++ app/quiz/forms.py
@@ -2,9 +2,12 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import Length
 
 
 class QuizForm(FlaskForm):
-    answer = StringField('Enter your translation', validators=[DataRequired(), Length(1, 128)])
+    answer = StringField(
+        'Enter your translation',
+        validators=[Length(1, 128, "Your translation was too short or too long.")]
+    )
     submit = SubmitField('Check')
