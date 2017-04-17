5c82aa2bd1628751e4f6647051342c9555ac5bca
merelinguist <merelinguist@users.noreply.github.com>
2017-04-17 22:38:15 +0100

    Add pie chart to topic page

--- app/topic/views.py
+++ app/topic/views.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, request, url_for
 from flask_login import current_user, login_required
 
 from app.topic import topic
-from app.models import Topic
+from app.models import Topic, Quiz
 
 
 def _get_user():
@@ -18,7 +18,21 @@ def topic(id):
 
     topic = Topic.query.filter_by(id=id).first_or_404()
 
+    quiz = Quiz.query.filter_by(id=user.quiz_id).first()
+
+    topic_size = len(topic.quizzes)
+
+    if quiz not in topic.quizzes:
+        progress = topic_size
+    else:
+        progress = topic.quizzes.index(quiz)
+
+    topic_progress = "%s" % (float(progress) / topic_size  * 100)
+
     return render_template(
         'topic.html',
-        topic=topic
+        topic=topic,
+        topic_progress=topic_progress,
+        progress=progress,
+        topic_size=topic_size,
     )
