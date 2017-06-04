cfb45d2008d59b28d0fad3842cfa18dad9b06147
merelinguist <merelinguist@users.noreply.github.com>
2017-06-04 01:05:15 +0100

    Move error page templates to own directory, fix content

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -8,18 +8,18 @@ from .. import api
 def forbidden(error):
     if request.path.startswith('/api'):
         return api.errors.forbidden(error)
-    return render_template('403.html'), 403
+    return render_template('error/403.html', title="LudoLatin - Forbidden"), 403
 
 
 @utils.app_errorhandler(404)
 def page_not_found(error):
     if request.path.startswith('/api'):
         return api.errors.not_found(error)
-    return render_template('404.html'), 404
+    return render_template('error/404.html', title="LudoLatin - Not found"), 404
 
 
 @utils.app_errorhandler(500)
 def internal_server_error(error):
     if request.path.startswith('/api'):
         return api.errors.internal_server_error(error)
-    return render_template('500.html'), 500
+    return render_template('error/500.html', title="LudoLatin - Server error"), 500
