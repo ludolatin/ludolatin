75f8cf516c3d5be4a8fceb7918b231fcd863fa1a
merelinguist <merelinguist@users.noreply.github.com>
2017-06-24 21:26:35 +0100

    Remove threads from email

--- app/email.py
+++ app/email.py
@@ -1,20 +1,11 @@
-from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
 
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-
 def send_email(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
     msg = Message('[' + current_app.config['SITE_NAME'] + '] ' + subject,
                   sender=current_app.config['MAIL_DEFAULT_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    mail.send(msg)
