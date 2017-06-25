7e8a026984f68d340c7ee6818a8a60d8cb1578c3
merelinguist <merelinguist@users.noreply.github.com>
2017-06-25 13:53:42 +0100

    Update database mgmt script

--- setup-db.sh
+++ setup-db.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # rm ingenuity-dev.db
-rm -r migrations/
+# rm -r migrations/
 flask db init
 flask db migrate
 flask db upgrade
 python manage.py load_data
 python manage.py add_admin
-python manage.py load_products
